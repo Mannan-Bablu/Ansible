d969735ceaad551ed6b06fd53f157ed08ce8ae77
mannan bablu <mannanbablu91@gmail.com>
2022-01-06 13:52:07 +0530

    changing the code

--- ec2-launch.sh
+++ ec2-launch.sh
@@ -1,4 +1,4 @@
 TEMP_ID="lt-02656782e3be9b697"
 TEMP_VER=2
 
-aws ec2 run-instance --launch-template LaunchTemplateId=${TEMP_ID},VERSION=${TEMP_VER} --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=frontend}]" "ResourceType=instance,Tags=[{Key=Name,Value=frontend}]" | jq
+aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},VERSION=${TEMP_VER} --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=frontend}]" "ResourceType=instance,Tags=[{Key=Name,Value=frontend}]" | jq
