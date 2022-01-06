8674cd3b394b5b73f9bc4723e2798e52f0000ed7
mannan bablu <mannanbablu91@gmail.com>
2022-01-06 12:58:55 +0530

    changing the code

--- ec2-launch.sh
+++ ec2-launch.sh
@@ -1,1 +1,1 @@
-aws ec2 request-spot-instances --instance-count 1 --type "persistent" --launch-specification file://spot.json --tag-specification "Resourcetype=spot-instances-request,Tags=[{Key=Name,Value=frontend}]"
+aws ec2 request-spot-instances --instance-count 1 --type "persistent" --launch-specification file://spot.json --tag-specification "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=frontend}]"
