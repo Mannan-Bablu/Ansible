dbfaff5f75c5aa93b8dc16e7d80799ac08e2cc06
mannan bablu <mannanbablu91@gmail.com>
2022-01-06 13:37:06 +0530

    changing the code

--- ec2-launch.sh
+++ ec2-launch.sh
@@ -1,1 +1,4 @@
-aws ec2 request-spot-instances --instance-count 1 --type "persistent" --launch-specification file://spot.json --tag-specification "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=frontend}]"
+TEMP_ID=lt-02656782e3be9b697
+TEMP_VER=2
+
+aws ec2 run-instance --launch-template LaunchTemplateId=${TEMP_ID},VERSION=${TEMP_VER} --tag-specifications "ResourceType=spot-instance-request,Tags=[{Key=Name,Value=frontend}]" "ResourceType=instance,Tags=[{Key=Name,Value=frontend}]" | jq
